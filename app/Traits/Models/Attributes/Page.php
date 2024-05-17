949ffa559fe5712f58dcf084a2a2c6e4f566da18
Shah Sawood <shahsawoodshinwari@gmail.com>
2024-05-18 01:48:24 +0500

    fixed exception

--- app/Traits/Models/Attributes/Page.php
+++ app/Traits/Models/Attributes/Page.php
@@ -13,7 +13,7 @@ trait Page
   public function setAttribute($key, $value)
   {
     if ($key === 'name' && !in_array($value, PageEnum::values())) {
-      throw new InvalidArgumentException("Invalid value `" . $value ."` for name. Valid values are: " . implode(', ', PageEnum::values()));
+      throw new InvalidArgumentException("Invalid value `" . $value?->value() . "` for name. Valid values are: " . implode(', ', PageEnum::values()));
     }
 
     return parent::setAttribute($key, $value);
