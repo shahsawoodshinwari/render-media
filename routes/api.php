a430489406fbfa07565034867651b7339dd9af0a
Shah Sawood <shahsawoodshinwari@gmail.com>
2024-06-04 20:26:06 +0500

    latest commits

--- routes/api.php
+++ routes/api.php
@@ -16,33 +16,35 @@ use App\Http\Controllers\Api\Auth\VerificationController;
 use App\Http\Controllers\Api\Auth\ResetPasswordController;
 use App\Http\Controllers\Api\Auth\ForgotPasswordController;
 
-Route::post('login', [LoginController::class, 'login']);
-Route::post('password/email', [ForgotPasswordController::class, 'sendResetLinkEmail']);
-Route::post('password/reset', [ResetPasswordController::class, 'reset']);
-Route::post('register', [RegisterController::class, 'register']);
-Route::post('become-freelancer', FreelanerController::class);
+Route::name('api.')->group(function () {
+  Route::post('login', [LoginController::class, 'login']);
+  Route::post('password/email', [ForgotPasswordController::class, 'sendResetLinkEmail']);
+  Route::post('password/reset', [ResetPasswordController::class, 'reset']);
+  Route::post('register', [RegisterController::class, 'register']);
+  Route::post('become-freelancer', FreelanerController::class);
 
-Route::resource('sub-categories', SubCategoryController::class)->only('index');
+  Route::resource('sub-categories', SubCategoryController::class)->only('index');
 
-Route::middleware('auth:sanctum')->group(function () {
-  Route::post('email/resend', [VerificationController::class, 'resend']);
-  Route::post('email/verify', [VerificationController::class, 'verify']);
-  Route::post('logout', [LoginController::class, 'logout']);
+  Route::middleware('auth:sanctum')->group(function () {
+    Route::post('email/resend', [VerificationController::class, 'resend']);
+    Route::post('email/verify', [VerificationController::class, 'verify']);
+    Route::post('logout', [LoginController::class, 'logout']);
 
-  Route::middleware('verified')->group(function () {
-    Route::post('update-password', PasswordController::class);
-    Route::post('update-profile-picture', [ProfileController::class, 'updateProfilePicture']);
-    Route::post('bookings/{booking}/reschedule', [BookingController::class, 'reschedule']);
+    Route::middleware('verified')->group(function () {
+      Route::post('update-password', PasswordController::class);
+      Route::post('update-profile-picture', [ProfileController::class, 'updateProfilePicture']);
+      Route::post('bookings/{booking}/reschedule', [BookingController::class, 'reschedule']);
 
-    Route::singleton('profile', ProfileController::class)->destroyable()->only(['show', 'update', 'destroy']);
+      Route::singleton('profile', ProfileController::class)->destroyable()->only(['show', 'update', 'destroy']);
 
-    Route::apiResource('bookings', BookingController::class)->only(['index', 'store']);
-    Route::apiResource('contact-us', ContactUsController::class)->only('store');
-    Route::apiResource('categories', CategoryController::class)->only('index');
+      Route::apiResource('bookings', BookingController::class)->only(['index', 'store']);
+      Route::apiResource('contact-us', ContactUsController::class)->only('store');
+      Route::apiResource('categories', CategoryController::class)->only('index');
 
-    Route::prefix('cms')->group(function () {
-      Route::apiResource('faqs', FAQController::class)->only('index');
-      Route::apiResource('pages/{page}', PageController::class)->only('index');
+      Route::prefix('cms')->group(function () {
+        Route::apiResource('faqs', FAQController::class)->only('index');
+        Route::apiResource('pages/{page}', PageController::class)->only('index');
+      });
     });
   });
 });
